c5e631d9076e1dd4277157e541e4d3aa8f34c35d
ssunnykku <ssunnykku@gmail.com>
2024-06-16 14:54:10 +0900

    test파일 추가

---
+++ src/main/java/com/test/demo/controller/testController.java
@@ -0,0 +1,12 @@
+package com.test.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class testController {
+    @GetMapping
+    public String get(){
+        return "test";
+    }
+}
