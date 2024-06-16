4bcb558c4794bf4b46a81d1f2d0c461a9076e6c4
ssunnykku <ssunnykku@gmail.com>
2024-06-16 16:47:36 +0900

    controller 경로 설정

--- src/main/java/com/test/demo/controller/testController.java
+++ src/main/java/com/test/demo/controller/testController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class testController {
-    @GetMapping
+    @GetMapping("/")
     public String get(){
         return "test";
     }
